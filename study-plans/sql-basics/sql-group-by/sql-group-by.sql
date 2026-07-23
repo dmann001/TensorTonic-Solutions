c36b7ba5e6b01e3825d8ad3c5820a8d8ac7fbefc
Dhruv <160906253+dmann001@users.noreply.github.com>
2026-07-23 08:32:20 -0400

    Solve sql-group-by

--- study-plans/sql-basics/sql-group-by/sql-group-by.sql
+++ study-plans/sql-basics/sql-group-by/sql-group-by.sql
@@ -1,4 +1,4 @@
-SELECT customer, COUNT(product) as total_orders, SUM(amount) as total_spent
+SELECT customer, COUNT(*) as total_orders, SUM(amount) as total_spent
 FROM orders
 GROUP BY customer
 ORDER BY total_spent DESC
