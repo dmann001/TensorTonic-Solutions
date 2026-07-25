914c140c455fde71841a37f69cc801a93894a086
Dhruv <160906253+dmann001@users.noreply.github.com>
2026-07-25 09:11:27 -0400

    Solve sql-conditional-aggregation

--- study-plans/sql-basics/sql-conditional-aggregation/sql-conditional-aggregation.sql
+++ study-plans/sql-basics/sql-conditional-aggregation/sql-conditional-aggregation.sql
@@ -1,9 +1,9 @@
 SELECT 
     department,
     COUNT(*) AS total_tickets, 
-    SUM(status= 'open') as open_count,
-    SUM(status= 'in_progress') as in_progress_count,
-    SUM(status= 'closed') as closed_count
+    SUM(CASE WHEN status= 'open' THEN 1 ELSE 0 END) as open_count,
+    SUM(CASE WHEN status= 'in_progress' THEN 1 ELSE 0 END) as in_progress_count,
+    SUM(CASE WHEN status= 'closed' THEN 1 ELSE 0 END) as closed_count
 FROM tickets
 GROUP BY department
 ORDER BY total_tickets DESC, department ASC;
