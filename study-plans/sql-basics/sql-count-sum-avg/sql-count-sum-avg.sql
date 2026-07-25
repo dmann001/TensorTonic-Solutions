a761f2faabf963f8d7ae909ced9ca859201fb0a9
Dhruv <160906253+dmann001@users.noreply.github.com>
2026-07-25 08:34:28 -0400

    Solve sql-count-sum-avg

--- study-plans/sql-basics/sql-count-sum-avg/sql-count-sum-avg.sql
+++ study-plans/sql-basics/sql-count-sum-avg/sql-count-sum-avg.sql
@@ -1,5 +1,8 @@
 -- Write your SQL query here
-SELECT category, COUNT(*) AS total_sales, SUM(AMOUNT) as total_revenue,AVG(discount) AS avg_discount
+SELECT category, 
+    COUNT(*) AS total_sales, 
+    SUM(AMOUNT) as total_revenue,
+    ROUND(AVG(discount),2) AS avg_discount
 FROM sales
 GROUP BY category
 ORDER BY total_revenue DESC, category ASC
